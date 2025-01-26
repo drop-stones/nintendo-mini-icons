0fe587cb563547ebf22b2b70746a55219923c250
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2025-01-26 10:23:35 +0900

    feat(clap): Add clap crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long, default_value_t = String::new())]
+    title: String,
+
+    #[arg(short, long, default_value_t = String::new())]
+    icon: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    if !args.title.is_empty() {
+        println!("Title: {}", args.title);
+    }
+
+    if !args.icon.is_empty() {
+        println!("Icon: {}", args.icon);
+    }
 }
